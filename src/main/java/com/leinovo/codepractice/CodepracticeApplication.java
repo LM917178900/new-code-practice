2e73f6653db79db8b08be9e9574b54bd320ff93a
leiming5 <leiming5@lenovo.com>
2021-09-23 09:16:44 +0800

    new code 67 prctice for algorithm

---
+++ src/main/java/com/leinovo/codepractice/CodepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.leinovo.codepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodepracticeApplication.class, args);
+    }
+
+}
