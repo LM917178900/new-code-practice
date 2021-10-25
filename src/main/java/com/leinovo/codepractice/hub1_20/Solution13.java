fa7c7f005df3db25a4e5b60b2b4bc773d8bf24b4
leiming5 <leiming5@lenovo.com>
2021-10-25 14:26:19 +0800

    robot routing test

--- src/main/java/com/leinovo/codepractice/hub1_20/Solution13.java
+++ src/main/java/com/leinovo/codepractice/hub1_20/Solution13.java
@@ -1,5 +1,8 @@
 package com.leinovo.codepractice.hub1_20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 描述
  * 地上有一个rows行和cols列的方格。坐标从 [0,0] 到 [rows-1,cols-1]。
@@ -8,7 +11,7 @@ package com.leinovo.codepractice.hub1_20;
  * 例如，当threshold为18时，机器人能够进入方格[35,37]，因为3+5+3+7 = 18。
  * 但是，它不能进入方格[35,38]，因为3+5+3+8 = 19。
  * 请问该机器人能够达到多少个格子？
- *
+ * <p>
  * 范围:
  * 1 <= rows, cols<= 100
  * 0 <= threshold <= 20
@@ -18,20 +21,45 @@ package com.leinovo.codepractice.hub1_20;
  * @date: 2021/10/22 13:34
  */
 public class Solution13 {
-    /**
-     *  1. 初次验证，如果rows/cols 满足返回(rows+1)*(cols+1)
-     *  2. 如果满足，开始减少列，直到满足，从这个点开始遍历；
-     *
-     * @param threshold
-     * @param rows
-     * @param cols
-     * @return
-     */
-    public int movingCount(int threshold, int rows, int cols) {
 
-        return 0;
+    public static void main(String[] args) {
+
+        int result = movingCount(15, 20, 20);
+        System.out.println(result);
     }
 
+    public static int movingCount(int threshold, int rows, int cols) {
+        List<String> allList = new ArrayList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 16 && j == 19) {
+                    System.out.println("stop here");
+                }
+                int row = getCount(i);
+                int col = getCount(j);
+
+                if (row + col <= threshold) {
+                    boolean routeTest = allList.size() == 0 || allList.contains((i - 1) + "-" + j) || allList.contains(i + "-" + (j - 1));
+                    if (routeTest) {
+                        allList.add(i + "-" + j);
+                    }
+                }
+            }
+        }
+
+        return allList.size();
+    }
+
+    private static int getCount(int number) {
+        String row = String.valueOf(number);
+        int result = 0;
+        char[] chars = row.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            result += Integer.parseInt(String.valueOf(chars[i]));
+        }
+        return result;
+    }
 
 
 }
